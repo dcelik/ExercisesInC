ec5ca17f5bd1afb6fe440c2bfa69a2d394330a61
Deniz Celik <deniz.celik@students.olin.edu>
2017-05-07 18:39:28 -0400

    Add ex12

--- exercises/ex12/counter_mutex.c
+++ exercises/ex12/counter_mutex.c
@@ -80,6 +80,7 @@ typedef struct {
     int counter;
     int end;
     int *array;
+    Semaphore *semaphore;
 } Shared;
 
 /*  make_shared
@@ -97,6 +98,7 @@ Shared *make_shared (int end)
 
     shared->counter = 0;
     shared->end = end;
+    shared->semaphore = make_semaphore(1);
   
     shared->array = check_malloc (shared->end * sizeof(int));
     for (i=0; i<shared->end; i++) {
@@ -154,18 +156,20 @@ void join_thread (pthread_t thread)
  */
 void child_code (Shared *shared)
 {
-    //printf ("Starting child at counter %d\n", shared->counter);
 
     while (1) {
+        sem_wait(shared->semaphore);
 	    if (shared->counter >= shared->end) {
+            sem_signal(shared->semaphore);
 	        return;
 	    }
 	    shared->array[shared->counter]++;
 	    shared->counter++;
 
 	    if (shared->counter % 100000 == 0) {
-	        //printf ("%d\n", shared->counter);
+	       //printf ("%d\n", shared->counter);
 	    }
+        sem_signal(shared->semaphore);
     }
 }
 
